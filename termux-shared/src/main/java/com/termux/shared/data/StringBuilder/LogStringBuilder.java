9e03df25947c6d2efdf6118eb88ef29ea8b57588
rubinstory@naver.com <rubinstory@naver.com>
2022-05-31 00:43:13 +0900

    Added: Create `ObjectStringBuilder` and `LogStringBuilder`
    - ObjectStringBuilder
        - This interface will provide better accessibility for Logger and
          MarkdownUtils.
    
    - LogStringBuilder
        - This class implements `ObjectStringBuilder` for providing better
          accessibilirt for Logger.

---
+++ termux-shared/src/main/java/com/termux/shared/data/StringBuilder/LogStringBuilder.java
@@ -0,0 +1,17 @@
+package com.termux.shared.data.StringBuilder;
+
+import com.termux.shared.logger.Logger;
+
+public class LogStringBuilder implements ObjectStringBuilder{
+    private StringBuilder builder = new StringBuilder();
+
+    @Override
+    public void appendSLEntry(String label, Object object) {
+        builder.append("\n").append(Logger.getSingleLineLogStringEntry(label, object, "-"));
+    }
+
+    @Override
+    public void appendMLEntry(String label, Object object) {
+        builder.append("\n").append(Logger.getMultiLineLogStringEntry(label, object, "-"));
+    }
+}
---
+++ termux-shared/src/main/java/com/termux/shared/data/StringBuilder/ObjectStringBuilder.java
@@ -0,0 +1,6 @@
+package com.termux.shared.data.StringBuilder;
+
+public interface ObjectStringBuilder {
+    public void appendSLEntry(String label, Object object);
+    public void appendMLEntry(String label, Object object);
+}
