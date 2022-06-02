fdbcc2690efaa23fac3ab146fb6ea02cff163638
cyj893 <cyj89317@gmail.com>
2022-06-03 00:18:50 +0900

    Added: test case for `getMarkdownCodeForStringEmpty()`
    Purpose: Test plain text and codeBlock
    Input: "test string"
    Expected:
         test string -> `test string`
         test string -> ```
                        test string
                        ```

--- termux-shared/src/test/java/com/termux/shared/markdown/MarkdownUtilsTest.java
+++ termux-shared/src/test/java/com/termux/shared/markdown/MarkdownUtilsTest.java
@@ -20,4 +20,21 @@ public class MarkdownUtilsTest extends TestCase {
         assertEquals(emptyStringResult, "");
     }
 
+    /**
+     * Purpose: Test plain text and codeBlock
+     * Input: "test string"
+     * Expected:
+     *      test string -> `test string`
+     *      test string -> ```
+     *                     test string
+     *                     ```
+     */
+    public void testGetMarkdownCodeForString() {
+        String plainTextResult = MarkdownUtils.getMarkdownCodeForString("test string", false);
+        assertEquals(plainTextResult, "`test string`");
+
+        String plainTextWithCodeBlockResult = MarkdownUtils.getMarkdownCodeForString("test string", true);
+        assertEquals(plainTextWithCodeBlockResult, "```\ntest string\n```");
+    }
+
 }
