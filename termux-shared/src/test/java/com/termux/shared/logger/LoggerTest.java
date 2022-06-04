f890710bf2c5614c61d9ed4e39de6d517a5c30c9
rubinstory@naver.com <rubinstory@naver.com>
2022-06-04 13:12:03 +0900

    Added: Add test case for Logger.getMessageAndStackStraceString()
    - Purpose
        - Check when throwable is given
    
    - Input
        - Logger.getMessageAndStackTraceString (null, Throwable), ("message", Throwable)
    
    - Expected
        - (null, Throwable) = Logger.getStackTraceString(Throwable)
        - ("message", Throwable) = "message:\n" + Logger.getStackTraceString(throwable)

--- termux-shared/src/test/java/com/termux/shared/logger/LoggerTest.java
+++ termux-shared/src/test/java/com/termux/shared/logger/LoggerTest.java
@@ -21,4 +21,24 @@ public class LoggerTest extends TestCase {
         String result = Logger.getMessageAndStackTraceString(message, null);
         assertEquals(result, message);
     }
+
+    /**
+     * Purpose: Check when throwable is given
+     * Input: Logger.getMessageAndStackTraceString (null, Throwable), ("message", Throwable)
+     * Expected:
+     *      (null, Throwable) = Logger.getStackTraceString(Throwable)
+     *      ("message", Throwable) = "message:\n" + Logger.getStackTraceString(throwable)
+     */
+    @Test
+    public void testGetMessageAndStackTraceStringForThrowable() {
+        String message = "message";
+        Throwable throwable = new Throwable();
+
+        String result = Logger.getMessageAndStackTraceString(null, throwable);
+        assertEquals(result, Logger.getStackTraceString(throwable));
+
+        result = Logger.getMessageAndStackTraceString(message, throwable);
+        String expected = message + ":\n" + Logger.getStackTraceString(throwable);
+        assertEquals(result, expected);
+    }
 }
