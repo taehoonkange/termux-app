5498479afd5522795b6fd1b988eb2b5d37e4aa19
cyj893 <cyj89317@gmail.com>
2022-06-05 20:25:30 +0900

    Added: test case for `getUriFileBasename()`
    Purpose: Check the each Uri made by {@link Uri#parse(String)} and {@link UriUtils#getFileUri(String)}
    Input: Uri.parse("file:///path1/path2#fragment"), UriUtils.getFileUri("/path1/path2#fragment"), true
    Expected: "path2#fragment"

--- termux-shared/src/androidTest/java/com/termux/shared/net/uri/UriUtilsTest.java
+++ termux-shared/src/androidTest/java/com/termux/shared/net/uri/UriUtilsTest.java
@@ -39,4 +39,17 @@ public class UriUtilsTest {
         assertEquals("/path1/path2#fragment", UriUtils.getUriFilePathWithFragment(uriWithParse), UriUtils.getUriFilePathWithFragment(uriWithBuilder));
     }
 
+    /**
+     * Purpose: Check the each Uri made by {@link Uri#parse(String)} and {@link UriUtils#getFileUri(String)}
+     * Input: Uri.parse("file:///path1/path2#fragment"), UriUtils.getFileUri("/path1/path2#fragment")
+     * Expected: "path2#fragment"
+     */
+    @Test
+    public void testGetUriFileBasenameUriWithFragment() {
+        Uri uriWithParse = Uri.parse("file:///path1/path2#fragment");
+        Uri uriWithBuilder = UriUtils.getFileUri("/path1/path2#fragment");
+        assertEquals("path2#fragment", UriUtils.getUriFileBasename(uriWithParse, true));
+        assertEquals("path2#fragment", UriUtils.getUriFileBasename(uriWithBuilder, true));
+    }
+
 }
